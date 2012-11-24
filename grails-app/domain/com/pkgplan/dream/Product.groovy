2e5d9fa9d05495454e0c3a95a3ad92d1eb26a05e
longhengyu <longhengyu@gmail.com>
2012-11-24 18:40:54 +0900

    add Product Domain

---
+++ grails-app/domain/com/pkgplan/dream/Product.groovy
@@ -0,0 +1,39 @@
+package com.pkgplan.dream
+
+class Product {
+
+    String name
+    String description
+    BigDecimal price // all price in CNY
+    Date dateCreated
+    int pYear = 0
+    int pMonth = 0
+    int pDay = 0
+    int pHour = 0
+    int pMinute = 0
+    int pSecond = 0
+    String status
+
+    // this code is the identification of each item which specified by the product owner (admin)
+    // we can use this code for reference of i18n information. for example for i18n name and description
+    // adding the translated description in i18n file, for example
+    // product.description.10001=Some Product Description.
+    // product.name.10001=SomeName
+    // usage will be
+    // <g:message code="product.description.${productInstance.code}"/>
+    String code
+
+    static constraints = {
+        name(blank: false)
+        code(blank: false)
+        description(maxSize: 1000)
+        price(nullable: false, min: new BigDecimal(0))
+        status(inList: ['OnSell','Hidden','OffSall'], blank: false)
+        pYear(min: 0)
+        pMonth(min: 0)
+        pDay(min: 0)
+        pHour(min: 0)
+        pMinute(min: 0)
+        pSecond(min: 0)
+    }
+}
