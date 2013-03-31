91c1f3bf55c4d7a29cda91e3aa81079bafdb0997
longhengyu <longhengyu@gmail.com>
2013-03-31 14:46:29 +0900

    add giftcard init data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,6 +3,7 @@ import com.pkgplan.dream.Server
 import javax.servlet.http.HttpServletRequest
 import ch.gstream.grails.plugins.dbunitoperator.DbUnitOperator
 import com.pkgplan.dream.Product
+import com.pkgplan.dream.Giftcard
 
 class BootStrap {
 
@@ -26,8 +27,13 @@ class BootStrap {
         }
 
         if (Product.count() == 0) {
-            //init server data
+            //init product data
             DbUnitOperator.operate("INSERT","data/init-product.xml")
+        }
+
+        if (Giftcard.count() == 0) {
+            //init giftcard data
+            DbUnitOperator.operate("INSERT","data/init-giftcard.xml")
         }
     }
     def destroy = {
