69efc644444a8db627477c746ed5f49ea27c5444
longhengyu <longhengyu@gmail.com>
2013-04-03 01:08:19 +0900

    add javascript to define the client timezone

--- grails-app/views/user/_showBody.gsp
+++ grails-app/views/user/_showBody.gsp
@@ -14,6 +14,7 @@
                         <div class="tab-content">
                             <g:render template="tabAccount"/>
                             <g:render template="tabProfile"/>
+
                         </div>
                     </div>
                 </div>
--- web-app/js/dream.js
+++ web-app/js/dream.js
@@ -5,4 +5,19 @@ $(function (){
     })
 
 
+    //By default, getTimezoneOffset returns the time zone offset in minutes
+    timediff = (-1)*new Date().getTimezoneOffset()/60;
+    setCookie("timezone", timediff);
+
+    function setCookie(name, value, expires, path, domain, secure)
+    {
+        document.cookie= name + "=" + escape(value) +
+            ((expires) ? "; expires=" + expires.toGMTString() : "") +
+            ((path) ? "; path=" + path : "") +
+            ((domain) ? "; domain=" + domain : "") +
+            ((secure) ? "; secure" : "");
+    }
+
+
 });
+
